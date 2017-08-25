8d85e9517bdb9545b1e11cd7a29f3355e3431be8
Aliaksei Lamaka <Aliaksei_Lamaka@epam.com>
2017-08-25 09:25:47 +0300

    First mini commit

--- TaskLogic/Customer.cs
+++ TaskLogic/Customer.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace TaskLogic
 {
-    public class Customer
+    public class Customer : IEquatable<Customer>
     {
+        #region Fields & properties
+
+        private int Id;
+        private string Name;
+        public string LastName;
+
+        #endregion
+
+        #region Equolity
+
+        public bool Equals(Customer other)
+        {
+            if (other == null)
+                throw new ArgumentNullException();
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Customer);
+
+        #endregion
+
+        #region Overrides from object
+
+        public override string ToString()
+        {
+            return $"Id of Customer: {Id}" + Environment.NewLine 
+                   + $"Name: {Name}" + Environment.NewLine
+                   + $"LastName: {LastName}";
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 13;
+
+                hash = (hash * 7) + Id.GetHashCode();
+                hash = (hash * 7) + Name.GetHashCode();
+                hash = (hash * 7) + LastName.GetHashCode();
+
+                return hash;
+            }
+        }
+
+        #endregion
+
     }
 }
